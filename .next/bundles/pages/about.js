28c05b5758d144826ebff743c4e0c1dc25547464
Yannick Panis <info@qucode.eu>
2017-07-26 01:16:33 +0200

    remove next

--- .next/bundles/pages/about.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([6],{105:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(1),o=n(l),r=u(67),a=n(r);t.default=function(){return o.default.createElement(a.default,null,o.default.createElement("p",null,"This is the About page"))}},221:function(e,t,u){e.exports=u(105)}},[221]);
-            return { page: comp.default }
-          })
-        
