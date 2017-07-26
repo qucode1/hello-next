8ff43aca9f581c788bb2f4be5d648cca24b3ffba
Yannick Panis <info@qucode.eu>
2017-07-26 02:54:03 +0200

    testing post deployment script & post-update hook

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import fetch from 'isomorphic-unfetch'
 const Index = (props) => (
   <Layout>
     <h1>Batman TV Shows</h1>
-    <h3>Testing git post-update & additional deploy script</h3>
+    <h3>Testing git post-update & additional deploy script again</h3>
     <ul>
       {props.shows.map(({show}) => (
         <li key={show.id}>
